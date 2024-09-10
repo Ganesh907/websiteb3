036a3d98411dd3170cce7b5da3a0339a70eae89d
Nkesh Gupta <nikeshgupta718@gmail.com>
2024-09-10 13:12:53 +0530

    Revert "NG(Techhomepage blinking code added)"
    This reverts commit 804b69ad0a79351d2046b07d46fe460d035f7bca.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,21 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       animation: {
-//         blink: 'blink 1s step-end infinite',
-//       },
-//       keyframes: {
-//         blink: {
-//           '50%': { opacity: 0 },
-//         },
-//       },
-//     },
-//   },
-//   plugins: [],
-// }
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path if your source files are located elsewhere
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       animation: {
-        'slow-blink': 'slow-blink 3s ease-in-out infinite', // Define a slower animation with smooth transitions
+        blink: 'blink 1s step-end infinite',
       },
       keyframes: {
-       'slow-blink': {
-          '0%, 100%': { opacity: 1 },  // Fully visible at the start and end
-          '50%': { opacity: 0 },       // Fully invisible in the middle
+        blink: {
+          '50%': { opacity: 0 },
         },
       },
     },
   },
   plugins: [],
 }
+
+
